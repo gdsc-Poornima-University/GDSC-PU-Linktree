6acb6de44fb0439d619def3d89d2a2233182d2a3
avayyyyyyy <jabhi465@gmail.com>
2023-08-31 14:37:55 +0530

    Update

--- data.js
+++ data.js
@@ -1,28 +1,20 @@
-// All Handles Data Object ****************************************************
+// Updated Handles Data Object ****************************************************
 
 let linktreeData = [
   {
     id: 1,
     handleName: "Chapter",
     handleText: "Become a Member",
-    handleLink:
-      "https://gdsc.community.dev/jalpaiguri-government-engineering-college-jalpaiguri/",
+    handleLink: "https://gdsc.community.dev/poornima-university-jaipur/",
     imgSrc: "./assets/community.svg",
     imgAlt: "Community",
-  },
-  {
-    id: 2,
-    handleName: "Discord",
-    handleText: "Join Our Community",
-    handleLink: "https://discord.gg/MxX4ZhPxKa",
-    imgSrc: "./assets/discord.svg",
-    imgAlt: "Discord",
   },
   {
     id: 3,
     handleName: "LinkedIn",
     handleText: "Connect Us & Stay Update",
-    handleLink: "https://www.linkedin.com/company/gdscjgec",
+    handleLink:
+      "https://www.linkedin.com/in/gdsc-poornima-university-34696528a/",
     imgSrc: "./assets/linkedin.svg",
     imgAlt: "LinkedIn",
   },
@@ -30,7 +22,7 @@ let linktreeData = [
     id: 4,
     handleName: "Youtube",
     handleText: "Subscribe to Our Channel",
-    handleLink: "https://www.youtube.com/c/gdscjgec",
+    handleLink: "https://www.youtube.com/@GDSCPoornimaUniversity-qf7fw",
     imgSrc: "./assets/youtube.svg",
     imgAlt: "Youtube",
   },
@@ -38,7 +30,7 @@ let linktreeData = [
     id: 5,
     handleName: "Instagram",
     handleText: "Follow Us & Stay Update",
-    handleLink: "https://www.instagram.com/gdscjgec/",
+    handleLink: "https://instagram.com/gdsc.poornima?igshid=NjIwNzIyMDk2Mg==",
     imgSrc: "./assets/instagram.svg",
     imgAlt: "Instagram",
   },
@@ -46,31 +38,15 @@ let linktreeData = [
     id: 6,
     handleName: "Github",
     handleText: "Collab with us in Our Projects",
-    handleLink: "https://github.com/gdscjgec",
+    handleLink: "https://github.com/gdsc-Poornima-University",
     imgSrc: "./assets/github.svg",
     imgAlt: "Github",
-  },
-  {
-    id: 7,
-    handleName: "Twitter",
-    handleText: "Follow us on Twitter",
-    handleLink: "https://twitter.com/gdscjgec",
-    imgSrc: "./assets/twitter.svg",
-    imgAlt: "Facebook",
-  },
-  {
-    id: 8,
-    handleName: "Facebook",
-    handleText: "Like our page on Facebook",
-    handleLink: "https://www.facebook.com/gdscjgec",
-    imgSrc: "./assets/facebook.svg",
-    imgAlt: "Facebook",
   },
   {
     id: 9,
     handleName: "Website",
     handleText: "Official Website",
-    handleLink: "https://gdscjgec.github.io/",
+    handleLink: "https://gdsc-poornima-university.github.io/GDSC-PU-Linktree/",
     imgSrc: "./assets/website.svg",
     imgAlt: "Website",
   },
@@ -78,13 +54,13 @@ let linktreeData = [
     id: 10,
     handleName: "Mail Us",
     handleText: "Feel Free to Contact Us",
-    handleLink: "mailto:gdsc@jgec.ac.in",
+    handleLink: "mailto:gdsc.poornimauniversity@gmail.com",
     imgSrc: "./assets/mail.svg",
     imgAlt: "Mail",
   },
 ];
 
-// All Handles Preview Mapping ****************************************************
+// Updated Handles Preview Mapping ****************************************************
 
 let linktreeDiv = document.querySelector("#linktreeHandles");
 
